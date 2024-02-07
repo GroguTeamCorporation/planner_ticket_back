0e1b99bb7289731e6d6f0b3067545b56891fc0c9
Borja <elbarmandev@gmail.com>
2024-02-07 15:16:07 +0100

    add cors

---
+++ src/main/java/dev/plannerticket/Controllers/UserController.java
@@ -0,0 +1,37 @@
+package dev.plannerticket.Controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.plannerticket.Models.User;
+import dev.plannerticket.Services.UserService;
+import io.micrometer.common.lang.NonNull;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}/users")
+public class UserController {
+
+     UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "")
+    public List<User> index () {
+        return service.getAll();
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<User> create(@NonNull @RequestBody User user) {
+        User newUser = service.save(user);
+        return ResponseEntity.status(201).body(newUser);
+    }
+    
+}
