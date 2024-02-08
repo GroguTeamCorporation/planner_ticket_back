41ac6b983897b8ead667ae5fa59370160945565e
Eva López <evamlopez1981@gmail.com>
2024-02-08 06:58:54 +0100

    modified Event CORS y data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,5 @@
-INSERT INTO events (title, date_event, time_event, capacity, description, image) VALUES
-  ('Evento 1', '2024-02-10', '18:00:00', 100, 'Descripción del evento 1', '/images/evento1.jpg');
+INSERT INTO events (id, title, description, location, capacity, date_event, time_event, image) VALUES
+  (default, 'Evento 1', 'Descripción del evento 1', 'Gijón', 38, '2024-02-10', '18:00:00', '/images/evento1.jpg');
 
-
-INSERT INTO events (title, date_event, time_event, capacity, description, image)
-VALUES ('Nebulossa', '2024-05-30', '20:00:00', 2000, 'lololooooooooooo piiiii tamagochi a dormir', '/images/evento1.jpg');
+INSERT INTO events (id, title, description, location, capacity, date_event, time_event, image)
+VALUES (default, 'Nebulossa', 'lololooooooooooo piiiii tamagochi a dormir', 'Oviedo', 216, '2024-05-30', '20:00:00', '/images/evento1.jpg');
